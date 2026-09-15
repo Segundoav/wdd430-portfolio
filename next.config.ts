875029442a5e7f9a62dd6d735247e7a108fa963d
Segundo Alvarez <segundoav@hotmail.com>
2026-09-15 00:34:26 -0500

    Fix basePath to only apply in production

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/wdd430-portfolio",
-  assetPrefix: "/wdd430-portfolio/",
+  basePath: isProd ? "/wdd430-portfolio" : "",
+  assetPrefix: isProd ? "/wdd430-portfolio/" : "",
   images: {
     unoptimized: true,
   },
